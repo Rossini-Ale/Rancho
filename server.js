d4cd5bdb6c9ad2e5147a7e764f300959aa2fd306
Alexandre José Rufca Rossini <alexandrerossini@MacBook-Rossini.local>
2026-02-26 19:30:07 -0300

    server

--- server.js
+++ server.js
@@ -33,7 +33,6 @@ app.get("*", (req, res) => {
 
 // IMPORTANTE: Adicionar '0.0.0.0'
 const PORT = process.env.PORT || 3000;
-
 app.listen(PORT, "0.0.0.0", () => {
-  console.log(`Servidor rodando em http://0.0.0.0:${PORT}`);
+  console.log(`Aplicação ativa na porta ${PORT}`);
 });
