adb6a55f8c37ac1e848e6826ba18c71dbccf4069
Alexandre José Rufca Rossini <alexandrerossini@MacBook-Rossini.local>
2026-02-26 17:57:50 -0300

    server

--- server.js
+++ server.js
@@ -26,6 +26,8 @@ app.get("*", (req, res) => {
 
 // Configuração da Porta exigida pela Hostinger
 const PORT = process.env.PORT || 3000;
+
+// IMPORTANTE: Adicionar '0.0.0.0'
 app.listen(PORT, "0.0.0.0", () => {
-  console.log(`Servidor rodando na porta ${PORT}`);
+  console.log(`Servidor a rodar na porta ${PORT}`);
 });
